0f5aced9e03af1bd73daa4345cf9e814f98da299
PratikWasekar <91455293+PratikWasekar@users.noreply.github.com>
2022-11-05 19:11:03 +0530

    Add files via upload

---
+++ My_First_Notebook_in_Watson.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# Bold text:__Pratik Balkrushna Wasekar__
+# Data Analyst
+
+# Italic text: _I am interested in data science because I want to use data science tools and techniques in optimizing marketing analytics.
+
+# ### The code will print "Hello! Welcome to Watson Studio!"
+
+# In[2]:
+
+
+print("Hello! Welcome to Watson Studio!")
+
+
+# 1. This is my first code in Python.
+# 2. This code will print the input text.
+# 
+# -  This is my first code in Python.
+# -  This code will print the input text.
+# 
+#  The code will
+#  ---
+# print
+# ---
+# "Hello!
+# ----
+# Welcome to Watson Studio!"
+# ---
+# 
+
+# In[ ]:
+
+
+
+
